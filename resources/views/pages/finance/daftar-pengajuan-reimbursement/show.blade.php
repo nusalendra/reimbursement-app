3870d3625fcbeb3df6dca3250c77d5d39ec993d2
Nusalendra <nusalendraa@gmail.com>
2024-05-09 10:32:24 +0700

    fix:change status setujui pengajuan to terima pengajuan

--- resources/views/pages/finance/daftar-pengajuan-reimbursement/show.blade.php
+++ resources/views/pages/finance/daftar-pengajuan-reimbursement/show.blade.php
@@ -48,10 +48,9 @@
                             value="Ditolak" name="tolak_pengajuan"
                             class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Tolak
                             Pengajuan</button>
-                        <button type="button" onclick="handlePengajuan('{{ $data->id }}', 'Disetujui')"
-                            value="Disetujui" name="setujui_pengajuan"
-                            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Setujui
-                            Pengajuan</button>
+                        <button type="button" onclick="handlePengajuan('{{ $data->id }}', 'Diterima')"
+                            value="Diterima" name="terima_pengajuan"
+                            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Terima Pengajuan</button>
                     </form>
                 </div>
             </div>
@@ -89,7 +88,7 @@
                     document.getElementById("reimbursementForm").submit();
                 }
             } else {
-                document.getElementById("status_pengajuan").value = 'Disetujui';
+                document.getElementById("status_pengajuan").value = 'Diterima';
                 document.getElementById("reimbursementForm").submit();
             }
         }
